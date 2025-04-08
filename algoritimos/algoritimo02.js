22204b43f3f75572204cf9b7789bd5c7c7e09880
VitorDioo <linklord.ll@gmail.com>
2025-04-08 17:31:28 -0300

    primeiro commit

---
+++ algoritimos/algoritimo02.js
@@ -0,0 +1,14 @@
+// algoritimo: Somar Dois números e mostras a resposta;
+
+console.log(3)
+console.log(4)
+console.log(3+4)
+
+// ---->
+
+var numero1 = 3;
+var numero2 = 4;
+var txt = "Resposta:"
+
+console.log(txt, numero1 + numero2)
+console.log(txt, numero1 - numero2)
